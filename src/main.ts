e25dcb31f0cb4a27b6a26cede09ed1e5d9faf214
SlimeyAr <lostbhijaya@gmail.com>
2024-09-28 20:52:43 +0700

    Working on some more tokennn

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import './style.css';
 import { main } from './sundae_theme.gleam';
+import { randomkt } from './randomkt.gleam';
 
 document.documentElement.innerHTML = main()
+// document.write(randomkt())
