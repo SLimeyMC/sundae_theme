0b5f5dfba0c44848b424159a522d120bc1af2509
SlimeyAr <lostbhijaya@gmail.com>
2024-10-15 21:14:59 +0700

    init..

--- farm.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@farmfe/core';
-import gleam from "vite-gleam";
-
-export default defineConfig({
-  compilation: {
-    presetEnv: false
-  },
-  vitePlugins: [gleam()]
-});
